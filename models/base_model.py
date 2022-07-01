77f8d78cc9699c807cb837d61b943f410fc7a1ef
J3rCast <jerson.castro.r@gmail.com>
2022-07-01 17:46:44 -0500

    add the posibility of re-create an instance with this dictionary representation.

--- models/base_model.py
+++ models/base_model.py
@@ -12,14 +12,23 @@ class BaseModel:
     """This is an abstract class that defines attributes
     for other classes.
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize the object
         Args:
             id (string): Unique id of the object
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for arg in kwargs:
+                if arg == '__class__':
+                    continue
+                if arg == 'created_at' or arg == 'updated_at':
+                    setattr(self, arg, datetime.fromisoformat(kwargs[arg]))
+                else:
+                    setattr(self, arg, kwargs[arg])
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         """Update the updated_at attribute.
