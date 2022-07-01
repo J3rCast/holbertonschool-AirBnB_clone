0b82582161d4dd8db7be321ebadf0760f97dac30
J3rCast <jerson.castro.r@gmail.com>
2022-07-01 10:05:36 -0500

    public attribute added

---
+++ models/base_model.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module contains a class BaseModel that defines all
+   common attributes/methods for other classes.
+"""
+
+
+class BaseModel:
+    """This is an abstract class that defines attributes
+    for other classes.
+    """
+    def __init__(self):
+        """Initialize the object
+        Args:
+            id (string): Unique id of the object
+        """
