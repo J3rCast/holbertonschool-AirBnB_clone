c64d32208751defb24f1feac2b173a8bc7862d00
J3rCast <jerson.castro.r@gmail.com>
2022-07-01 12:50:20 -0500

    to_dict method added and __str__ magic method added

--- models/base_model.py
+++ models/base_model.py
@@ -16,12 +16,28 @@ class BaseModel:
         Args:
             id (string): Unique id of the object
         """
-        self.id = uuid.uuid4()
-        self.created_at = datetime()
-        self.updated_at = datetime()
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def save(self):
+        """Update the updated_at attribute.
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary containing all
+        keys/values of __dict__ of the instance
+        """
+        new_dict = self.__dict__
+        new_dict['__class__'] = type(self).__name__
+        new_dict['created_at'] = new_dict['created_at'].isoformat()
+        new_dict['updated_at'] = new_dict['updated_at'].isoformat()
+        return new_dict
 
     def __str__(self):
         """Magic method that return a string to use
         when print function is called.
         """
-        pass
+        return "[{}] ({}) {}".format(type(self).__name__,\
+            self.id, self.__dict__)
