2e112429744233aa91dd4bb14de02ce7e36d5832
J3rCast <jerson.castro.r@gmail.com>
2022-07-05 11:46:16 -0500

    amenity file added

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module contains a class
+called amenity.
+"""
+
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """This class inherits from BaseModel."""
+    name = ""
