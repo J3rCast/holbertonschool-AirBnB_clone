ef1a77c1e394ef0889ad47d50e8feb0a5e20fc23
J3rCast <jerson.castro.r@gmail.com>
2022-07-05 11:42:56 -0500

    test files added

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""This is the test module for review.py"""
+
+
+from models.base_models import BaseModel
---
+++ tests/test_models/test_user.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""This is the test module for state.py"""
+
+
+from models.base_models import BaseModel
